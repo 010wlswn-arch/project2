795c30869439d1278d9c32b8d13fc38c6048d722
010wlswn-arch <010wlswn@gmail.com>
2026-08-19 17:43:28 +0900

    Add files via upload

---
+++ 05_family/js/main.js
@@ -0,0 +1,12 @@
+// jQuery
+$(function () {
+
+    // Family Site 버튼을 클릭하면
+    $(".family-btn").click(function () {
+
+        // Family Site 목록을 열기 / 닫기
+        $(".family-list").slideToggle();
+
+    });
+
+});
