e4b1a816084bd329a624f5ef42885a4ab612b5b9
010wlswn-arch <010wlswn@gmail.com>
2026-08-27 15:09:23 +0900

    Add files via upload

--- busan-food-app/js/config.js
+++ busan-food-app/js/config.js
@@ -2,7 +2,7 @@
 const APP_CONFIG={
   BUSAN_DATA_API_KEY:"wqTU18g0nW%2BWEvPmkt%2FDRxJ71CdFjqe4VI9B9DzF1TjZOKI6I0UazNdj2lN4fIzGX9Arb4Tpj%2F56KGsotRjSMg%3D%3D",
   KAKAO_JAVASCRIPT_KEY:"cdc4a945f8e2d325d1b2c9a5baec7db3",
-  USE_SAMPLE_DATA:true,
+  USE_SAMPLE_DATA:false,
   API_BASE_URL:"https://apis.data.go.kr/6260000/FoodService/getFoodKr",
   IMAGE_BASE_URL:"https://www.visitbusan.net"
 };
