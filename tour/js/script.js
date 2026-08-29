fe4f90a314e950e5a3f13ad3fbe19c47cffaa7ea
010wlswn-arch <010wlswn@gmail.com>
2026-08-29 16:21:02 +0900

    Add files via upload

---
+++ tour/js/script.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    
+    $(".gnb>li").mouseenter(function(){
+        $(".depth2, .box").stop().fadeIn();
+    });
+    $(".gnb").mouseleave(function(){
+        $(".depth2, .box").stop().fadeOut();
+    });
+    setInterval(function(){
+
+        $(".slide ul").animate(
+            {top: "-=300px"},
+            "slow",
+            function(){
+                $(".slide ul li").first().appendTo(".slide ul");
+                $(".slide ul").css("top", 0);
+            }
+        );
+
+    }, 3000);
+    // family 버튼을 클릭하면 한번 보이고/ 숨김
+    $("button").click(function(){
+        $(".list").fadeToggle();
+    })
+
+
+
+});
